cdf1ec762986c70eb29a517f253a0ba773d6058a
abhiattri <15abhiattri@gmail.com>
2022-05-21 09:52:48 +0530

    my app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Tempapp from './components/Tempapp';
-import react from "react";
+import React from "react";
 import './App.css';
 
 function App() {
--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -1,4 +1,4 @@
-import react, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './css/style.css';
 
 
